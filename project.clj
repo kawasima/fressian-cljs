74f2b3ab5e31c64f021556ef89e8a29df0531850
kawasima <kawasima1016@gmail.com>
2014-02-12 09:30:49 +0900

    Changed a scope of clojurescript to provided.

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject net.unit8/fressian-cljs "0.1.0"
-  :dependencies [[org.clojure/clojurescript "0.0-2156"]]
+  :dependencies [[org.clojure/clojurescript "0.0-2156" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.0.2"]
             [com.cemerick/clojurescript.test "0.2.2"]]
+  :source-paths ["src/clj" "src/cljs"]
   :cljsbuild
   { :builds 
-    {:dev
+    {:product
       { :source-paths ["src/cljs"]
+        :jar true
         :compiler { :optimizations :whitespace
                     :output-to "target/fressian.js"}}
       :test
